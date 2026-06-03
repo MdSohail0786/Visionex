da20c78626e7333849686df57daa2f32f754a020
Md Sohail <mdsohail1575426@gmail.com>
2026-06-04 01:41:23 +0530

    edit vercel file

--- landing-page/vite.config.js
+++ landing-page/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
+{
+  "buildCommand": "npm run build",
+  "outputDirectory": "dist",
+  "installCommand": "npm install",
+  "framework": "vite",
+  "rewrites": [{ "source": "/(.*)", "destination": "/index.html" }]
+}
